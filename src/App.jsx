734b845b2fb80c50b08206014c898a97b09a161d
Adithyan Valloor <vallooradithyan@gmail.com>
2024-11-08 19:47:25 +0530

    Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import AboutPage from './AboutPage'
 import Expertise from './Expertise'
 import Contact from './Contact'
 import Footer from './Footer'
-import Test from './Test'
 
 function App() {
   return (
     <>
-      {/* <Test/> */}
       <Header/>
       <Homepage/>
       <AboutPage/>
